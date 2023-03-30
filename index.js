828cab71cf7d52cdbc80d47303202407468a0da0
Arman Habib <arman@seoprotected.com>
2023-03-30 11:25:55 +0600

    commit_003

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 console.log(456132);
 
-function calculateBMI() {
-			// Get input values
-			var weight = document.getElementById("weight").value;
-			var height = document.getElementById("height").value;
-
-			// Convert height to meters
-			height /= 100;
-
-			// Calculate BMI
-			var bmi = weight / (height * height);
-
-			// Display result
-			document.getElementById("result").innerHTML = bmi.toFixed(2);
-		}
